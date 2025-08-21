6fcf8ca82f07eaabac76f38740e5610c6d55b377
Matheus Alencar <matheusalencar6942@gmail.com>
2025-08-21 16:02:30 -0300

    adicionando autenticação nas rotas de agentes e casos

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const { AppError } = require("../utils/errorHandler");
+
+const SECRET = process.env.JWT_SECRET || "secret";
+
+function authenticateToken(req, res, next) {
+  const cookieToken = req.cookies?.token;
+  const authHeader = req.headers["authorization"];
+  const headerToken = authHeader && authHeader.split(" ")[1];
+
+  const token = cookieToken || headerToken;
+
+  if (!token) {
+    throw new AppError(401, "Token não fornecido.");
+  }
+
+  jwt.verify(token, SECRET, (err, user) => {
+    if (err) {
+      throw new AppError(403, "Token inválido ou expirado.");
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = { authenticateToken };
--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const PORT = process.env.PORT || 3000;
 const agentesRouter = require("./routes/agentesRoutes");
 const casosRouter = require("./routes/casosRoutes");
 const authRouter = require("./routes/authRoutes");
+const { authenticateToken } = require("./middlewares/authMiddleware");
 const { errorHandler } = require("./utils/errorHandler");
 
 app.use(express.json());
 
-app.use(casosRouter);
-app.use(agentesRouter);
 app.use(authRouter);
+app.use(authenticateToken, casosRouter);
+app.use(authenticateToken, agentesRouter);
 
 swagger(app);
 
