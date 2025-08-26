54a24442743450fbd48980fe7e5ff42c39452fae
Matheus Alencar <matheusalencar6942@gmail.com>
2025-08-26 18:04:13 -0300

    deixando como antes

--- repositories/agentesRepository.js
+++ repositories/agentesRepository.js
@@ -7,7 +7,12 @@ async function findAll(filter = {}, orderBy = ["id", "asc"]) {
       .select("*")
       .where(filter)
       .orderBy(orderBy[0], orderBy[1]);
-    return result;
+    return result.map((agente) => ({
+      ...agente,
+      dataDeIncorporacao: new Date(agente.dataDeIncorporacao)
+        .toISOString()
+        .split("T")[0],
+    }));
   } catch (error) {
     throw new AppError(500, "Erro ao buscar agentes", [error.message]);
   }
