2fde8b1c6fa31394d76bd17ae2d2a9a387df3c91
Matheus Alencar <matheusalencar6942@gmail.com>
2025-08-27 21:12:02 -0300

    ajustando middleware de autenticação

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ const { AppError } = require("../utils/errorHandler");
 
 const SECRET = process.env.JWT_SECRET || "secret";
 
-function authenticateToken(req, res, next) {
+async function authenticateToken(req, res, next) {
   const cookieToken = req.cookies?.token;
   const authHeader = req.headers["authorization"];
   const headerToken = authHeader && authHeader.split(" ")[1];
