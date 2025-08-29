941e43ec453f0e864e8ecd781f537cbfdc3d9d8c
Matheus Alencar <matheusalencar6942@gmail.com>
2025-08-29 16:41:02 -0300

    adicionando try catch

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,26 +4,30 @@ const { AppError } = require("../utils/errorHandler");
 const SECRET = process.env.JWT_SECRET || "secret";
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  try {
+    const authHeader = req.headers["authorization"];
 
-  if (!authHeader) {
-    return next(new AppError(401, "Token não fornecido."));
-  }
-
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) {
-    return next(new AppError(401, "Token não fornecido."));
-  }
-
-  jwt.verify(token, SECRET, (err, user) => {
-    if (err) {
-      return next(new AppError(401, "Token inválido ou expirado."));
+    if (!authHeader) {
+      return next(new AppError(401, "Token não fornecido."));
     }
 
-    req.user = user;
-    return next();
-  });
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+      return next(new AppError(401, "Token não fornecido."));
+    }
+
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return next(new AppError(401, "Token inválido ou expirado."));
+      }
+
+      req.user = user;
+      return next();
+    });
+  } catch (e) {
+    return next(new AppError(401, "Token inválido ou expirado."));
+  }
 }
 
 module.exports = { authenticateToken };
